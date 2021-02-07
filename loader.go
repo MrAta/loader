48215f4f09a0d73063ecb6096a53b249f3d2e164
Ata Fatahi <immrata@gmail.com>
2021-02-06 21:49:33 -0500

    buf fixes with lables

--- loader.go
+++ loader.go
@@ -22,7 +22,7 @@ func main() {
 	flag.Parse()
 
 	duration := getDuration(*durationPtr)
-	
+
 	file, err := os.OpenFile(*fileNamePtr, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
@@ -33,29 +33,39 @@ func main() {
 	fmt.Println("Start: ", time.Now())
 	ticker := time.NewTicker(time.Second)
 	done := make(chan bool)
-	wg := sync.WaitGroup{}
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case <-ticker.C:
-				go sendReq(addr, *ratePtr, &wg, time.Now(), logger)
-			}
-		}
-	}()
+	finishedLaunch := make(chan bool)
 
+	go launchWorkers(ratePtr, done, finishedLaunch, ticker, logger)
 	time.Sleep(duration)
 	ticker.Stop()
 	done <- true
-	wg.Wait()
+
+	<-finishedLaunch
 	fmt.Println("End: ", time.Now())
 }
-func sendReq(addr string, rate int, wg *sync.WaitGroup, t time.Time, logger *log.Logger) {
-	for i := 0; i < rate; i++ {
-		go func(addr string, l *log.Logger, t time.Time) {
+
+func launchWorkers(ratePtr *int, done chan bool, finishedLaunch chan bool, ticker *time.Ticker, logger *log.Logger) {
+	var wg sync.WaitGroup
+	for {
+		select {
+		case <-done:
+			goto WaitForWorkers
+		case <-ticker.C:
 			wg.Add(1)
-			defer wg.Done()
+			go sendReq(addr, *ratePtr, &wg, time.Now(), logger)
+		}
+	}
+WaitForWorkers:
+	wg.Wait()
+	finishedLaunch <- true
+}
+func sendReq(addr string, rate int, wg *sync.WaitGroup, t time.Time, logger *log.Logger) {
+
+	var reqWg sync.WaitGroup
+	for i := 0; i < rate; i++ {
+		reqWg.Add(1)
+		go func(addr string, l *log.Logger, t time.Time, reqWg *sync.WaitGroup) {
+			defer reqWg.Done()
 			resp, err := http.Get(addr)
 			var statusCode int
 			if err != nil {
@@ -66,9 +76,10 @@ func sendReq(addr string, rate int, wg *sync.WaitGroup, t time.Time, logger *log
 
 			l.Println(t, statusCode)
 			//fmt.Println(t, resp.StatusCode)
-		}(addr, logger, t)
+		}(addr, logger, t, &reqWg)
 	}
-
+	reqWg.Wait()
+	wg.Done()
 }
 
 func getDuration(d string) time.Duration {
