e00d8c838c91195867f3e55823248349cbe1fe10
Ata Fatahi <immrata@gmail.com>
2021-02-06 21:50:26 -0500

    more coarse rates

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-duration=1m
-for rate in 1 10 100; do
+duration=5s
+for rate in 100 500 1000 1500 2000 2500 3000; do
     echo "Running at rate "$rate"...";
     ./loader -duration=$duration -rate=$rate -logfile=info$rate-$duration.log
 done
